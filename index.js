d5880217c87d3996572246ee11fc118ac6beaf40
julian David Arias <SENA@ARMAPRCCIFOD610.FormacionSena.RED>
2024-03-06 07:18:51 -0500

    array combinado

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ let arrayTiposMixtos = ["x",7,"true"];
 let arrayConstructor = new array("abc");
 let arraySecuencia = array.from({leght:5},(_,index)=> String.fromCharCode(97+index));
 let arrayRepetido = new array(3).fill("r");
-let cadena = "los arrays son fabulosos"
-let arrayDesdeCadena = cadena.split("")
+let cadena = "los arrays son fabulosos";
+let arrayDesdeCadena = cadena.split("");
+let array1 =["a","b"];
+let array2 =["c","d"];
+let arrayCombinado =[...array1,...array2];
