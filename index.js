7738cce45cfe9d103d9c3248435ac8da1b24ef5e
julian David Arias <SENA@ARMAPRCCIFOD610.FormacionSena.RED>
2024-03-06 07:12:13 -0500

    array cadena

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let arrayTiposMixtos = ["x",7,"true"];
 let arrayConstructor = new array("abc");
 let arraySecuencia = array.from({leght:5},(_,index)=> String.fromCharCode(97+index));
 let arrayRepetido = new array(3).fill("r");
+let cadena = "los arrays son fabulosos"
+let arrayDesdeCadena = cadena.split("")
