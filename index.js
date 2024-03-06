c82848f9396ab376f7782b2ad4fa621a61d8be04
julian David Arias <SENA@ARMAPRCCIFOD610.FormacionSena.RED>
2024-03-06 06:52:15 -0500

    array secuencia numerica

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ let arrayVacio = [];
 let arrayElementos = ["a","b","c"];
 let arrayTiposMixtos = ["x",7,"true"];
 let arrayConstructor = new array("abc");
+let arraySecuencia = array.from({leght:5},(_,index)=> String.fromCharCode(97+index));
