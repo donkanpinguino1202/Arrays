48296bf16952829d1ef2894aaafad366c6febf84
julian David Arias <SENA@ARMAPRCCIFOD610.FormacionSena.RED>
2024-03-06 06:57:50 -0500

    array repeticion

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let arrayElementos = ["a","b","c"];
 let arrayTiposMixtos = ["x",7,"true"];
 let arrayConstructor = new array("abc");
 let arraySecuencia = array.from({leght:5},(_,index)=> String.fromCharCode(97+index));
+let arrayRepetido = new array(3).fill("r");
